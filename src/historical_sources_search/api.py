b33b4f8d78faca5a756e3c0f6123e1a2efa12c95
Ryan Echols <rkechols21@gmail.com>
2025-08-09 01:52:21 -0600

    basic infrastructure for searching multiple source collections

--- src/historical_sources_search/api.py
+++ src/historical_sources_search/api.py
@@ -2,6 +2,10 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from historical_sources_search.search import Search
+from historical_sources_search.search_result import SearchResult
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,3 +23,19 @@ api = FastAPI(lifespan=_lifespan)
 @api.get("/")
 async def get_root() -> dict[str, str]:
     return {"status": "ok"}
+
+
+class SearchRequest(BaseModel):
+    query: str
+
+
+class SearchResponse(BaseModel):
+    query: str
+    results: list[SearchResult]
+
+
+@api.post("/search")
+async def post_search(request: SearchRequest) -> SearchResponse:
+    search = Search(request.query)
+    await search.execute()
+    return SearchResponse(query=search.query, results=search.results)
---
+++ src/historical_sources_search/search.py
@@ -0,0 +1,28 @@
+import logging
+
+from historical_sources_search.search_result import SearchResult
+from historical_sources_search.source_collections.base import SourceCollectionBase
+from historical_sources_search.source_collections.example_collection import ExampleCollection
+
+LOGGER = logging.getLogger(__name__)
+
+SOURCE_COLLECTIONS: list[SourceCollectionBase] = [
+    ExampleCollection("https://example.com"),
+]
+
+
+class Search:
+    def __init__(self, query: str):
+        super().__init__()
+        self.query = query
+        self._results: list[SearchResult] = []
+
+    @property
+    def results(self) -> list[SearchResult]:
+        return self._results
+
+    async def execute(self):
+        # TODO: use a queue and workers
+        for source_collection in SOURCE_COLLECTIONS:
+            async for result in source_collection.search(self.query):
+                self._results.append(result)
