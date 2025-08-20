bf4f1273dd14fd6e23af68c0f390b7e4a6827e91
Ryan Echols <rkechols21@gmail.com>
2025-08-19 19:00:08 -0600

    make linter happy

--- search-ui/src/App.tsx
+++ search-ui/src/App.tsx
@@ -6,19 +6,19 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function incrementCount() {
+    setCount((count) => count + 1)
+  }
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={incrementCount}>
           count is {count}
         </button>
         <p>
