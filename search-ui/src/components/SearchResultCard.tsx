66c42333d7823a4f689e1a88a493718b2bf0e946
Ryan Echols <rkechols21@gmail.com>
2025-08-22 18:42:04 -0600

    extra div

--- search-ui/src/components/SearchResultCard.tsx
+++ search-ui/src/components/SearchResultCard.tsx
@@ -8,8 +8,10 @@ export default function SearchResultCard({ searchResult }: SearchResultCardProps
   return <a href={searchResult.url} target="_blank" rel="noopener noreferrer">
     <div>
       {!!searchResult.image_src && (<img src={searchResult.image_src} alt={searchResult.title || "no caption"} />)}
-      {searchResult.title || searchResult.url}
-      {!!searchResult.detail && (<p>{searchResult.detail}</p>)}
+      <div>
+        {searchResult.title || searchResult.url}
+        {!!searchResult.detail && (<p>{searchResult.detail}</p>)}
+      </div>
     </div>
   </a>
 }
