bb7d009c78671aca409fa0c3967f049dc8886e10
Ryan Echols <rkechols21@gmail.com>
2025-08-22 21:58:48 -0600

    rearrange card contents

--- search-ui/src/components/SearchResultCard.tsx
+++ search-ui/src/components/SearchResultCard.tsx
@@ -8,14 +8,16 @@ export default function SearchResultCard({ searchResult }: SearchResultCardProps
   return (
     <a className="result-card-link" href={searchResult.url} target="_blank" rel="noopener noreferrer">
       <div className="result-card">
-        {!!searchResult.image_src && (
-          <img src={searchResult.image_src} alt={searchResult.title || "no caption"} />
-        )}
-        <div className="result-card-content">
+        <div>
           <div className="result-card-title">{searchResult.title || searchResult.url}</div>
-          {!!searchResult.detail && (
-            <div className="result-card-detail">{searchResult.detail}</div>
-          )}
+          <div className="result-card-content">
+            {!!searchResult.image_src && (
+              <img src={searchResult.image_src} alt={searchResult.title || "no caption"} />
+            )}
+            {!!searchResult.detail && (
+              <div className="result-card-detail">{searchResult.detail}</div>
+            )}
+          </div>
         </div>
       </div>
     </a>
