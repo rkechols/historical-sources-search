dbc80e2cc3d7e756d2142c807a2e1748a48ae4f9
Ryan Echols <rkechols21@gmail.com>
2025-08-23 01:56:07 -0600

    more rearranging

--- search-ui/src/components/SearchResultCard.tsx
+++ search-ui/src/components/SearchResultCard.tsx
@@ -8,17 +8,20 @@ export default function SearchResultCard({ searchResult }: SearchResultCardProps
   return (
     <a className="result-card-link" href={searchResult.url} target="_blank" rel="noopener noreferrer">
       <div className="result-card">
-        <div>
-          <div className="result-card-title">{searchResult.title || searchResult.url}</div>
-          <div className="result-card-content">
-            {!!searchResult.image_src && (
-              <img src={searchResult.image_src} alt={searchResult.title || "no caption"} />
-            )}
-            {!!searchResult.detail && (
-              <div className="result-card-detail">{searchResult.detail}</div>
-            )}
-          </div>
+        <div className="result-card-title">{searchResult.title || searchResult.url}</div>
+        <div className="result-card-detail">
+          {!!searchResult.image_src && (
+            <img src={searchResult.image_src} alt={searchResult.title || "no caption"} />
+          )}
+          {!!searchResult.detail && (
+            <p>{searchResult.detail}</p>
+          )}
         </div>
+        <p className="result-card-source">
+          Source Collection: <a href={searchResult.provided_by_collection.url} target="_blank" rel="noopener noreferrer">
+            {searchResult.provided_by_collection.name}
+          </a>
+        </p>
       </div>
     </a>
   )
